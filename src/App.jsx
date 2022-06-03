8b61316c10013f616ccf49b7cf70e7a0200aece3
fabiof2x <fabioar@gmail.com>
2022-06-03 15:53:40 -0300

    Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { Header } from "./components/Header"
+import { Header } from './components/Header';
 
-import './global.css'
+import styles from './App.module.css';
+import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <>
       <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          posts
+        </main>
+      </div>
     </>
   )
 }
