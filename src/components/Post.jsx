b1f8b4feb2b2afa419842e708b268580a8c7bb5f
fabiof2x <fabioar@gmail.com>
2022-06-04 11:37:05 -0300

    Estilizações do Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -13,15 +13,17 @@ export function Post() {
         </div>
 
         <time title='04 de junho às 11:04h' dateTime='2022-06-04 11:04:20'>Publicado há 1h</time>
-
-        <div className={styles.content}>
-          <p>Fala galeraa 👋</p>
-          <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-          <p>👉 <a href="#">jane.design/doctorcare</a></p>
-          <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
-        </div>
-
       </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p><a href="#">jane.design/doctorcare</a></p>
+        <p>
+          <a href="#">#novoprojeto</a>{' '}
+          <a href="#">#nlw</a>{' '}
+          <a href="#">#rocketseat</a></p>
+      </div>
     </article>
   )
 }
